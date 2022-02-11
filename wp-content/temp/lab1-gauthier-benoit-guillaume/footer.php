a4602dd5e46804aa73050f12cf9e450f2941b04b
Guillaume <guillaume.gb@live.ca>
2022-02-11 17:41:18 -0500

    Changement de l'emplacement des commits apres avoir realiser qu'ils n'etaient pas au bon endroit

--- wp-content/temp/lab1-gauthier-benoit-guillaume/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="centre">
-        <section class="footer__liens">Lien</section>
-        <section class="footer__adresses">Adresse</section>
-        <section class="foot__articles">Article</section>
-    </div>
-</footer>
-<?php wp_footer();?>
