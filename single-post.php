514b0defceaa6fbb04b44a208948ba25cc3a51c7
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-03-02 18:59:35 -0500

    Modification de l'apparence du single-post, modification de la taille du sous-titre du site et remise de l'exercice 2

--- single-post.php
+++ single-post.php
@@ -1,14 +1,13 @@
 <?php 	get_header() ?>
-<main>
+<main class="main__single-post">
     <h1>--------------- single-post.php ---------------</h1>
     <?php 	if (have_posts()) : while(have_posts()): the_post(); ?>
         <article class="cours">
-            <?php 	the_post_thumbnail() ?>
+            <?php 	the_post_thumbnail('medium') ?>
             <h2 class="cours__titre">
                 <?php 	the_title() ?>
             </h2>
                 <section>
-                    <?= 	the_post_thumbnail('medium') ?>
                     <?= 	get_the_content() ?>
                 </section>
         </article>
