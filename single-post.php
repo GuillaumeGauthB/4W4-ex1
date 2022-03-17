5d934a5b8beb87faa1f6b7ec59530c23042d82fe
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-03-16 20:57:16 -0400

    Correction de quelques bugs

--- single-post.php
+++ single-post.php
@@ -1,5 +1,5 @@
 <?php 	get_header() ?>
-<main class="main__single-post">
+<main class="main__single-post .principal">
     <h1>--------------- single-post.php ---------------</h1>
     <?php 	if (have_posts()) : while(have_posts()): the_post(); ?>
         <article class="cours">
