6a3494d1bccdd968814fed1955e6a63447ec4b35
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-04-12 17:32:35 -0400

    s11-c1 Modification du contenu de la page 404

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php 	get_header() ?>
-<main class="site__main">
+<main class="site__main main__404">
     <section class="erreur-404">
     <h1>Page existe pas</h1>
     <h2>Prend un lien de la barre de recherche</h2>
