d60054ac2da60ded72e93d279337c8eacacd7557
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-04-12 18:41:34 -0400

    s11-c1 Imprimer le logo et modification de header__titre

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <body  <?php body_class("site"); ?>>
 <header class="site__header">
     <section class="titre">
-        <h1 class="header__titre"> <a href="<?php echo esc_url( home_url('/'));?>"><?php 	the_custom_logo() //get_custom_logo ?><?php bloginfo('name'); ?> </a> </h1>
+        <h1 class="header__titre"> <a href="<?php echo esc_url( home_url('/'));?>"><?php 	the_custom_logo(); ?></a> <a href="<?php echo esc_url( home_url('/'));?>"><?php bloginfo('name'); ?></a></h1>
         <h2 class="header__description"><?php bloginfo('description'); ?></h2>
     </section>
     <div class="util">
