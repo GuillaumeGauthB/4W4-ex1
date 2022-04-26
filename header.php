f4a0d7b0cc2537f6050e4a1cd9c913a0a05c85c3
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-04-26 19:56:42 -0400

    s12-c1 enlever le ">

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
         }
     </style>
 </head>
-<body  <?php body_class("site"); ?> style="background-color:<?= get_theme_mod('background_body') ?>;"?>">
+<body  <?php body_class("site"); ?> style="background-color:<?= get_theme_mod('background_body') ?>;"?>
 <header class="site__header">
 <div class="boite__modale boite__modale--fermer">
     <button class="boite__modale__fermer">X</button>
