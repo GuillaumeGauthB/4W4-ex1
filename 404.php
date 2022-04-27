c298fbe961c76f3c805f49858f1a5a2d10284cbf
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-04-26 21:33:15 -0400

    modification de la page 404

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php 	get_header() ?>
 <main class="site__main main__404">
     <section class="erreur-404">
-    <h1>Page existe pas</h1>
-    <h2>Prend un lien de la barre de recherche</h2>
-    <p>Arrete de faire n'importe quoi</p>
+    <h1>oh non... la page n'existe plus</h1>
+    <h2>On dirait que Channel l'a mangé...</h2>
+    <p>Vous devriez peut être partir, avant que qu'elle ne vous mange aussi</p>
     </section>
 </main>
 <?php 	get_footer() ?>
