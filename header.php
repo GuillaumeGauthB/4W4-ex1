e0030fb7f91739d28f65ad7cd9c1db3741ec8778
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-04-26 21:09:14 -0400

    s12-c1 deplacement du bouton de la boite modale

--- header.php
+++ header.php
@@ -20,10 +20,10 @@
 <body  <?php body_class("site"); ?> style="background-color:<?= get_theme_mod('background_body') ?>;"?>
 <header class="site__header">
 <div class="boite__modale boite__modale--fermer">
-    <button class="boite__modale__fermer">X</button>
     <article class="boite__modale__texte">
         Ceci est un test pour verifier le fonctionnement de la boite modale
     </article>
+    <button class="boite__modale__fermer">X</button>
 </div>
     <section class="titre">
         <h1 class="header__titre"> <a href="<?php echo esc_url( home_url('/'));?>"><?php 	the_custom_logo(); ?></a> <a href="<?php echo esc_url( home_url('/'));?>"><?php bloginfo('name'); ?></a></h1>
