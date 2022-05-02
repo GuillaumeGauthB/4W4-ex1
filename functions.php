56408806073c89831225420b2a65adf26eda152c
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-02 12:36:27 -0400

    Ajout du menu burger

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function cidw_4w4_enqueue(){
     wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
     wp_enqueue_style('cidw-4w4-police-google', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Palette+Mosaic&family=Ubuntu+Mono&display=swap', false);
     wp_enqueue_script('cidw-4w4-boite-modale', get_template_directory_uri() . '/scripts/boite-modale.js', array(), filemtime(get_template_directory() . '/scripts/boite-modale.js'), true);
+    wp_enqueue_script('cidw-4w4-burger', get_template_directory_uri() . '/scripts/burger.js', array(), filemtime(get_template_directory() . '/scripts/burger.js'), true);
 }
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
 
@@ -165,6 +166,7 @@ function prefix_nav_description( $item_output, $item,  $args ) {
     if ( !empty( $item->description ) ) {
         $item_output = str_replace( $args->link_after . '</a>',
         $args->link_after .'<hr><span class="menu-item-description">' . $item->description . '</span>' .  '</a>',
+        '<hr><span class="menu-item-description">' . $item->description. '</span></a>',
               $item_output );
     }
 }
