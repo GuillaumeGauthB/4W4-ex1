8cd9d37074077172ce65ef2d21a5bc47d3146e54
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-03 15:49:45 -0400

    s13-c1 modification de la taille de l'image et du bouton pour fermer la boite modale

--- scripts/caroussel.js
+++ scripts/caroussel.js
@@ -3,15 +3,17 @@
     let boite__modale = document.querySelector(".boite__modale")
     let galerie__img = document.querySelectorAll('.galerie .wp-block-image img')
     console.log("galerie__img.length = " + galerie__img.length)
-    let boite__modale__ferme = document.querySelector(".boite__modale--fermer")
+    let boite__modale__ferme = document.querySelector(".boite__modale__fermer")
     let boite__modale__texte = document.querySelector(".boite__modale__texte")
   
     boite__modale__ferme.addEventListener('mousedown', function(){
       boite__modale.classList.remove('boite__modale--ouvrir')
+      boite__modale.classList.add('boite__modale--fermer')
     })
   
     let image = document.createElement('img')
     boite__modale__texte.appendChild(image)
+    image.classList.add("boite__modale__img")
     //img.setAttribute("src", "#")
   console.log("test");
   
