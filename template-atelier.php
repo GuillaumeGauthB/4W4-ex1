243d6e7d7e46864520a6577e1f25db042d8f6d75
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-19 13:02:17 -0400

    ef - modification du style de template-atelier

--- template-atelier.php
+++ template-atelier.php
@@ -16,15 +16,15 @@
                <h2 class="atelier__titre"><?php the_title() ?></h2>
                <!-- <p>Informations pertinentes</p> -->
                <div class="atelier__temps">
-                    <p class="atelier__container__animateur">Date de début: <?php 	the_field("date_debut") ?></p>
-                    <p class="atelier__container__animateur">Date de fin: <?php 	the_field("date_fin") ?></p>
-                    <p class="atelier__container__animateur">Heure du début de l'atelier: <?php 	the_field("heure_debut") ?></p>
-                    <p class="atelier__container__animateur">Heure de fin de l'atelier: <?php 	the_field("heure_fin") ?></p>
-                    <p class="atelier__container__animateur">Durée d'une séance: <?php 	the_field("duree_dune_seance") ?></p>
+                    <p class="atelier__temps__date-debut">Date de début: <?php 	the_field("date_debut") ?></p>
+                    <p class="atelier__temps__date-fin">Date de fin: <?php 	the_field("date_fin") ?></p>
+                    <p class="atelier__temps__heure-debut">Heure du début de l'atelier: <?php 	the_field("heure_debut") ?></p>
+                    <p class="atelier__temps__heure-fin">Heure de fin de l'atelier: <?php 	the_field("heure_fin") ?></p>
+                    <p class="atelier__temps__duree">Durée d'une séance: <?php 	the_field("duree_dune_seance") ?></p>
                </div>
-               <p class="atelier__container__animateur">Animateur: <?php 	the_field("animateur") ?></p>
-               <p class="atelier__container__animateur">Local: <?php 	the_field("local") ?></p>
-               <p class="atelier__container__animateur">Description: <?php 	the_field("description") ?></p>
+               <p class="atelier__animateur">Animateur: <?php 	the_field("animateur") ?></p>
+               <p class="atelier__local">Local: <?php 	the_field("local") ?></p>
+               <p class="atelier__description">Description: <?php 	the_field("description") ?></p>
           </div>
           <div>
           </div>
