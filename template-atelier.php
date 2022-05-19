80838bcb58f43dde5cb9291a6b961e975800e4fd
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-19 12:50:20 -0400

    ef - modification de l'ordre d'imprimage de template-atelier

--- template-atelier.php
+++ template-atelier.php
@@ -14,18 +14,17 @@
           <div>
           <?php if (have_posts()): ?>
                <h2 class="atelier__titre"><?php the_title() ?></h2>
-               <p>Informations pertinentes</p>
-               <div class="atelier__container">
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("animateur") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("date_fin") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("jours_de_semaine") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("heure_debut") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("heure_fin") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("duree_dune_seance") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("local") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("date_debut") ?></p>
-                    <p class="atelier__container__animateur">Animateur: <?php 	the_field("description") ?></p>
+               <!-- <p>Informations pertinentes</p> -->
+               <div class="atelier__temps">
+                    <p class="atelier__container__animateur">Date de début: <?php 	the_field("date_debut") ?></p>
+                    <p class="atelier__container__animateur">Date de fin: <?php 	the_field("date_fin") ?></p>
+                    <p class="atelier__container__animateur">Heure du début de l'atelier: <?php 	the_field("heure_debut") ?></p>
+                    <p class="atelier__container__animateur">Heure de fin de l'atelier: <?php 	the_field("heure_fin") ?></p>
+                    <p class="atelier__container__animateur">Durée d'une séance: <?php 	the_field("duree_dune_seance") ?></p>
                </div>
+               <p class="atelier__container__animateur">Animateur: <?php 	the_field("animateur") ?></p>
+               <p class="atelier__container__animateur">Local: <?php 	the_field("local") ?></p>
+               <p class="atelier__container__animateur">Description: <?php 	the_field("description") ?></p>
           </div>
           <div>
           </div>
