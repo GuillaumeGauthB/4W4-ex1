964ffb53bafa298363572a5de65ca90aa420c3ec
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-19 14:21:56 -0400

    ef - modification de l'apparence de template-atelier

--- template-atelier.php
+++ template-atelier.php
@@ -12,18 +12,18 @@
      <article class="atelier">
           <div>
           <?php if (have_posts()): ?>
-               <h1 class="atelier__titre"><?php the_title() ?></h1>
+               <h1 class="atelier atelier__titre"><span class="texte__atelier"><?php the_title() ?></span></h1>
+               <p class="atelier atelier__description">Description: <span class="texte__atelier"><?php 	the_field("description") ?></span></p>  
+               <p class="atelier atelier__animateur">Animateur: <span class="texte__atelier"><?php 	the_field("animateur") ?></span></p>
+               <p class="atelier atelier__local">Local: <span class="texte__atelier"><?php 	the_field("local") ?></span></p>
                <!-- <p>Informations pertinentes</p> -->
-               <div class="atelier__temps">
-                    <p class="atelier__temps__date-debut">Date de début: <?php 	the_field("date_debut") ?></p>
-                    <p class="atelier__temps__date-fin">Date de fin: <?php 	the_field("date_fin") ?></p>
-                    <p class="atelier__temps__heure-debut">Heure du début de l'atelier: <?php 	the_field("heure_debut") ?></p>
-                    <p class="atelier__temps__heure-fin">Heure de fin de l'atelier: <?php 	the_field("heure_fin") ?></p>
-                    <p class="atelier__temps__duree">Durée d'une séance: <?php 	the_field("duree_dune_seance") ?></p>
+               <div class="atelier atelier__temps">
+                    <p class="atelier atelier__temps__date-debut">Date de début: <span class="texte__atelier"><?php 	the_field("date_debut") ?></span></p>
+                    <p class="atelier atelier__temps__date-fin">Date de fin: <span class="texte__atelier"><?php 	the_field("date_fin") ?></span></p>
+                    <p class="atelier atelier__temps__heure-debut">Heure du début de l'atelier: <span class="texte__atelier"><?php 	the_field("heure_debut") ?></span></p>
+                    <p class="atelier atelier__temps__heure-fin">Heure de fin de l'atelier: <span class="texte__atelier"><?php 	the_field("heure_fin") ?></span></p>
+                    <p class="atelier atelier__temps__duree">Durée d'une séance: <span class="texte__atelier"><?php 	the_field("duree_dune_seance") ?></span></p>
                </div>
-               <p class="atelier__animateur">Animateur: <?php 	the_field("animateur") ?></p>
-               <p class="atelier__local">Local: <?php 	the_field("local") ?></p>
-               <p class="atelier__description">Description: <?php 	the_field("description") ?></p>
           </div>
           <div>
           </div>
